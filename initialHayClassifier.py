91d0df62e3696089f85ccb870bb9398f2e189b43
Evan Peter Blake <evan.peter.blake@gmail.com>
2018-06-14 01:21:45 +1000

    Uploading initial scripts for the hay classifier
    The network is a bastardised version of darknet-19. Current model is able to detect whether or not there's a hay bale in an image 90.7%-ish of the time.

---
+++ initialHayClassifier.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+import os         
+from random import shuffle 
+from tqdm import tqdm      
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+#HAY_DIR = 'Subset/YesHayResized'
+#NOTHAY_DIR = 'Subset/NotHayResized'
+IMG_SIZE = 128
+LR = 1e-6
+MODEL_NAME = 'hay-convnet'
+epochs = 250
+save_dir = os.path.join(os.getcwd(),'saved_models')
+model_dir= 'saved_models/tf_hayClassifierYOLO_model.h5'
+
+#x_placeholder = tf.placeholder(tf.float32, [1,128,128,3])
+
+def load_image(img,hayDir):
+    #img = '4.jpg'
+    path = os.path.join(hayDir,img)
+    print(path)
+    img_data = cv2.imread(path,1)
+    img_data = cv2.resize(img_data,(IMG_SIZE,IMG_SIZE))
+
+    display_image(img_data)
+    return img_data
+
+def display_image(img_data):
+    #Display sample picture
+    fig = plt.figure(figsize=(2,2))
+    plt.imshow(img_data)
+    plt.show()
+
+def build_network():
+    tf.reset_default_graph()
+    convnet = input_data(shape=[IMG_SIZE, IMG_SIZE, 3], name='input')
+    convnet = conv_2d(convnet, 32, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+    convnet = conv_2d(convnet, 64, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 128, 3, activation='relu')
+    convnet = conv_2d(convnet, 64, 1, activation='relu')
+    convnet = conv_2d(convnet, 128, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 256, 3, activation='relu')
+    convnet = conv_2d(convnet, 128, 1, activation='relu')
+    convnet = conv_2d(convnet, 256, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = conv_2d(convnet,256,1, activation='relu')
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = conv_2d(convnet,256,1, activation='relu')
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+    convnet = conv_2d(convnet,512,1, activation='relu')
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+    convnet = conv_2d(convnet,512,1, activation='relu')
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    return convnet
+
+model = tflearn.DNN(build_network(), tensorboard_dir='log', tensorboard_verbose=0)
+model.load(model_dir)
+
+def predimg(imgdir,hayDir):
+    x = load_image(imgdir,hayDir)
+    x = np.expand_dims(x,0)
+
+    prediction = model.predict(x)
+    prediction = np.rint(prediction)
+    print(prediction)
---
+++ tensorflownetwork3.py
@@ -0,0 +1,116 @@
+import cv2
+import numpy as np
+import os         
+from random import shuffle 
+from tqdm import tqdm      
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.activations import softmax
+from tflearn.layers.estimator import regression
+
+
+HAY_DIR = 'Subset/YesHayResized'
+NOTHAY_DIR = 'Subset/NotHayResized'
+IMG_SIZE = 128
+LR = 1e-6
+MODEL_NAME = 'hay-convnet'
+epochs = 250
+save_dir = os.path.join(os.getcwd(),'saved_models')
+
+model_name = 'tf_hayClassifierYOLO_model.h5'
+
+def create_train_data():
+    x = []
+    for img in tqdm(os.listdir(HAY_DIR)):
+        path = os.path.join(HAY_DIR, img)
+        img_data = cv2.imread(path, 1)
+        img_data = cv2.resize(img_data, (IMG_SIZE, IMG_SIZE))
+        img_data_flipped = cv2.flip(img_data, 1)
+        x.append([np.array(img_data), np.array([1,0])])
+        x.append([np.array(img_data_flipped), np.array([1,0])])
+
+    for img in tqdm(os.listdir(NOTHAY_DIR)):
+        path = os.path.join(NOTHAY_DIR, img)
+        img_data = cv2.imread(path, 1)
+        img_data = cv2.resize(img_data, (IMG_SIZE, IMG_SIZE))
+        img_data_flipped = cv2.flip(img_data, 1)
+        x.append([np.array(img_data), np.array([0,1])])
+        x.append([np.array(img_data_flipped), np.array([0,1])])
+
+    shuffle(x)
+    testSetSize = np.floor_divide(len(x),10)
+    testing_data = x[0:testSetSize]
+    training_data = x[testSetSize:]
+
+    np.save('train_data.npy', training_data)
+    np.save('test_data.npy',testing_data)
+    return training_data,testing_data
+
+# If dataset is not created:
+#train_data,test_data = create_train_data()
+# If you have already created the dataset:
+train_data = np.load('train_data.npy')
+test_data = np.load('test_data.npy')
+
+X_train = np.array([i[0] for i in train_data]).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+y_train = [i[1] for i in train_data]
+X_test = np.array([i[0] for i in test_data]).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+y_test = [i[1] for i in test_data]
+
+def build_network():
+    tf.reset_default_graph()
+    convnet = input_data(shape=[IMG_SIZE, IMG_SIZE, 3], name='input')
+    convnet = conv_2d(convnet, 32, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+    convnet = conv_2d(convnet, 64, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 128, 3, activation='relu')
+    convnet = conv_2d(convnet, 64, 1, activation='relu')
+    convnet = conv_2d(convnet, 128, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 256, 3, activation='relu')
+    convnet = conv_2d(convnet, 128, 1, activation='relu')
+    convnet = conv_2d(convnet, 256, 3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = conv_2d(convnet,256,1, activation='relu')
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = conv_2d(convnet,256,1, activation='relu')
+    convnet = conv_2d(convnet,512,3, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+    convnet = conv_2d(convnet,512,1, activation='relu')
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+    convnet = conv_2d(convnet,512,1, activation='relu')
+    convnet = conv_2d(convnet,1024,3, activation='relu')
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    return convnet
+
+model = tflearn.DNN(build_network(), tensorboard_dir='log', tensorboard_verbose=0)
+model.fit({'input': X_train}, {'targets': y_train}, n_epoch=epochs, 
+          validation_set=({'input': X_test}, {'targets': y_test}), 
+          snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir,model_name)
+model.save(model_path)
+print('Saved trained model at %s' % model_path)
+
+"""
+plt.plot(np.squeeze(costs))
+plt.ylabel('cost')
+plt.xlabel('iterations (per tens)')
+plt.title("Learning rate =" + str(LR))
+plt.show()
+"""
